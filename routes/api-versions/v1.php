e939a86c27f9eae99d8346c109eac8e5b66f27f8
oong <mkhalil26122000@gmail.com>
2026-02-23 11:27:27 +0700

    Update v1.php

--- routes/api-versions/v1.php
+++ routes/api-versions/v1.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\API\v1\AttendanceController;
-use App\Http\Controllers\API\v1\AuthController;
+use App\Http\Controllers\API\V1\AttendanceController;
+use App\Http\Controllers\API\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api-key'])
